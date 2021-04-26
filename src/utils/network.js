033cdefda9e5fe1284cd4cc09dce1c3a62a0e974
Carolina <carolinaurrea_8@hotmail.com>
2021-04-26 19:25:13 -0400

    Fixed networing

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 
-
 const network = () => {
     if (window.location.origin === "https://genocafe-client.herokuapp.com/"){
-        return REACT_APP_SEVER_PROD_URL
+        return process.env.REACT_APP_SEVER_PROD_URL
     } else {
-   return REACT_APP_SEVER_URL
+   return process.env.REACT_APP_SEVER_URL
     }
 }
 export default network;
