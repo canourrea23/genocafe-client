b23084d2052913a15b1a91a7642e5f826f56bb3f
Carolina <carolinaurrea_8@hotmail.com>
2021-06-08 19:19:49 -0500

    backend connected

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const network = () => {
-    if (window.location.origin === "https://genocafe-client.herokuapp.com/"){
-        return process.env.REACT_APP_SEVER_PROD_URL
-    } else {
-   return process.env.REACT_APP_SEVER_URL
-    }
-}
+  if (window.location.origin === "https://genocafe-client.herokuapp.com/") {
+    return process.env.REACT_APP_SEVER_PROD_URL;
+  } else {
+    return process.env.REACT_APP_SEVER_URL;
+  }
+};
 export default network;
